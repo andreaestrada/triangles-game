b7ae66cd5a5806fc5bad34b58399f144efbf231d
Andrea <andrea9973@gmail.com>
2017-11-04 14:02:33 -0400

    Implemented triangle click

--- RotateTriangle.py
+++ RotateTriangle.py
@@ -7,6 +7,7 @@ def init(data):
     data.size = 100 #side length of triangle
     data.x = data.width/2 #bottom left x starting position
     data.y = 200 #bottom left y starting position
+    data.color = "black"
 
     #animate triangle
     data.rotateDeg = 2 #rotation speed 
@@ -22,7 +23,7 @@ def init(data):
     data.centroid = [(coor1[0] + coor2[0] + coor3[0])/3, (coor1[1] + coor2[1] + coor3[1])/3]
 
 def redrawAll(canvas, data):
-    canvas.create_polygon(data.triangle, fill = "black") #draw triangle
+    canvas.create_polygon(data.triangle, fill = data.color) #draw triangle
 
 def timerFired(data):
     data.triangle = rotateTriangle(data.rotateDeg, data.centroid, data.triangle)
@@ -43,7 +44,20 @@ def moveTriangle(centroid, triangle, dropSpeed):
         coordinate[1] += dropSpeed
     centroid[1] += dropSpeed
 
-def mousePressed(event, data): pass
+def mousePressed(event, data):
+    mouseCoors = (event.x, event.y)
+    if triangleHit(mouseCoors, data.triangle): data.color = "red"
+
+def triangleHit(pt,tri):
+    #checks if mouse click is in triangle
+    a = 1/(-tri[1][1]*tri[2][0]+tri[0][1]*(-tri[1][0]+tri[2][0])+ 
+        tri[0][0]*(tri[1][1]-tri[2][1])+tri[1][0]*tri[2][1])
+    s = a*(tri[2][0]*tri[0][1]-tri[0][0]*tri[2][1]+(tri[2][1]-tri[0][1])*pt[0]+ \
+        (tri[0][0]-tri[2][0])*pt[1])
+    if s<0: return False
+    else: t = a*(tri[0][0]*tri[1][1]-tri[1][0]*tri[0][1]+(tri[0][1]-tri[1][1])*pt[0]+ \
+              (tri[1][0]-tri[0][0])*pt[1])
+    return ((t>0) and (1-s-t>0))
 
 def keyPressed(event,data): pass
 
